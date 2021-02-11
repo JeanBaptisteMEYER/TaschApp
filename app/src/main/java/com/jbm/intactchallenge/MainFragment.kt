daf96b6ac428856237b05eef1a6907033bccb6ee
Jean-Baptiste Meyer <meyer.jeanbaptiste@gmail.com>
2021-02-11 12:08:22 -0500

    fist Commit

---
+++ app/src/main/java/com/jbm/intactchallenge/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.jbm.intactchallenge
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.main_fragment, container,false)
+
+        return view
+    }
+
+}
